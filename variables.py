5da7eee9be35ab749309e06d6a7bc939c756613f
me-hapi <118534362+me-hapi@users.noreply.github.com>
2024-05-04 15:08:41 +0000

    ask age and tell the age in 5 years

--- variables.py
+++ variables.py
@@ -1,6 +1,5 @@
 #Asks age and it will tell your age in 5 years
 
 age = int(input('Enter your age: '))
-
 age_in_five_years = age+5
 print(f'You will be {age_in_five_years} years old in five years')
